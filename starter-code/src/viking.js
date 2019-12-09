3f1688becfcb13f787d4969469b0838d24e2c8f6
Robelo Steven <20299@columbushs.com>
2019-12-09 11:16:21 -0500

    commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,40 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+    return this.strength
+    }
+    receiveDamage(theDamage){
+        this.health = this.health - theDamage;
+        
+    }
 
-// Viking
-class Viking {}
 
-// Saxon
-class Saxon {}
-
-// War
-class War {}
+  }
+  
+  // Viking
+  class Viking extends Soldier {
+      constructor (name, health, strength) {
+          super(health, strength)
+          this.name = name;
+      }
+      receiveDamage(theDamage){
+          this.health = this.health - theDamage;
+          if (this.health > 0){
+            return this.name + ' has received ' + this.damage + ' points of damage';
+        }
+        else{
+            return this.name + ' has died in act of combat';
+        }
+          
+      }
+  }
+  
+  // Saxon
+  class Saxon {}
+  
+  // War
+  class War {}
