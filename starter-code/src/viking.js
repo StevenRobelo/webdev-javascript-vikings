aca14681cf17b5077094708c98ad0979c697406b
Robelo Steven <20299@columbushs.com>
2019-12-10 11:19:28 -0500

    commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -24,17 +24,49 @@ class Soldier {
       receiveDamage(theDamage){
           this.health = this.health - theDamage;
           if (this.health > 0){
-            return this.name + ' has received ' + this.damage + ' points of damage';
+            return `${this.name} has received ${theDamage} points of damage`;
         }
-        else{
-            return this.name + ' has died in act of combat';
+        else {
+            return `${this.name} has died in act of combat`;
+        }
+      }
+        battleCry() {
+            return 'Odin Owns You All!';
         }
           
       }
-  }
+  
   
   // Saxon
-  class Saxon {}
+  class Saxon extends Soldier {
+      constructor (health, strength) {
+          super (health, strength)
+      }
+      receiveDamage(theDamage){
+        this.health = this.health - theDamage;
+        if (this.health > 0){
+          return `A Saxon has received ${theDamage} points of damage`;
+        }
+        else {
+            return 'A Saxon has died in combat';
+        }
+      }
+      
+  }
   
   // War
-  class War {}
+  class War {
+      constructor() {
+          this.vikingArmy = [];
+          this.saxonArmy = [];
+      }
+      addViking(vikingObject){
+          this.vikingArmy.push(vikingObject);
+      }
+      addSaxon(saxonObject){
+          this.saxonArmy.push(saxonObject);
+      }
+      vikingAttack(){
+          
+      }
+  }
